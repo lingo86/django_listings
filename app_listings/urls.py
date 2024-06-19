68d760487d5ebeba2fe015e61b9a98b44421791a
lingo86 <aunindo.cs@gmail.com>
2024-06-19 16:42:32 +0530

    all_listings

--- app_listings/urls.py
+++ app_listings/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from . import views
 
 app_name = 'listings' # helps Django identify and choose the correct urls.py file from other urls.py files in the project’s directory
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", views.index, name="index"),
+    path("all_listings/", views.all_listings, name="all_listings"),
 ]
--- app_listings/views.py
+++ app_listings/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Listings
 
 # Create your views here.
 def index(request):
     return render(request, 'listings/index.html')
+
+def all_listings(request):
+    listings = Listings.objects.order_by('-list_date')
+    context = {'listings': listings}
+    return render(request, 'listings/all_listings.html', context)
